03fd1c982beb08590181803f060a2af8c8f1dd1c
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-25 18:30:49 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+    :weight, :handed, :complexion, :t_shirt_size,
+    :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(properties={})
+    properties.each do |k, pv| self.send("#{k}=", pv) end
+  end
 end
